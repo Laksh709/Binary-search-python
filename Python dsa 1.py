fd053dcdaff5bceefb7cebea5f499b4ff408b70e
Laksh709 <laksh3836@gmail.com>
2026-07-30 20:45:12 +0530

    Some minor changes and defining the binary search function

--- Python dsa 1.py
+++ Python dsa 1.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[69]:
 
 
 def locate_card(cards,query):
     pass
 
 
-# In[2]:
+# In[70]:
 
 
 cards = [ 13,11,12,7,4,3,1,0]
@@ -16,20 +16,20 @@ query = 7
 output = 3
 
 
-# In[3]:
+# In[71]:
 
 
 result = locate_card(cards,query)
 print(result)
 
 
-# In[4]:
+# In[72]:
 
 
 result == output
 
 
-# In[5]:
+# In[73]:
 
 
 test = {
@@ -41,19 +41,19 @@ test = {
 }
 
 
-# In[6]:
+# In[74]:
 
 
 locate_card(**test['input']) == test['output']
 
 
-# In[7]:
+# In[75]:
 
 
 tests = []
 
 
-# In[8]:
+# In[76]:
 
 
 tests.append(test)
@@ -67,7 +67,7 @@ tests.append({
 })
 
 
-# In[9]:
+# In[77]:
 
 
 #cards contain query as the first element
@@ -80,7 +80,7 @@ tests.append({
 })
 
 
-# In[10]:
+# In[78]:
 
 
 #cards contain only one element ,that is query itself
@@ -93,7 +93,7 @@ tests.append({
 })
 
 
-# In[11]:
+# In[79]:
 
 
 #cards contain query at the end of the list
@@ -106,7 +106,7 @@ tests.append({
 })
 
 
-# In[12]:
+# In[80]:
 
 
 #cards does not contain the carry abd we assumne -1 to be the output in all such cases 
@@ -119,7 +119,7 @@ tests.append({
 })
 
 
-# In[13]:
+# In[81]:
 
 
 #cards is empty
@@ -132,7 +132,7 @@ tests.append({
 })
 
 
-# In[14]:
+# In[82]:
 
 
 tests.append({
@@ -144,7 +144,7 @@ tests.append({
 })
 
 
-# In[15]:
+# In[83]:
 
 
 #query can occur multiple times , we expect the function to return the position of the first query
@@ -157,29 +157,16 @@ tests.append({
 })
 
 
-# In[16]:
-
-
-#there can be multiple numbers in cards
-tests.append({
-    'input': {
-        'cards' : [2,2,2,2,3,3,4,4,4,5,6,6],
-        'query' : 5
-},
-        'output': 9
-})
-
-
-# In[17]:
+# In[84]:
 
 
 tests
 
 
-# In[18]:
+# In[85]:
 
 
-def locate_card(cards, query):
+def locate_card_linear(cards, query):
     position = 0 
 
     while True :
@@ -193,59 +180,59 @@ def locate_card(cards, query):
 
 
 
-# In[19]:
+# In[86]:
 
 
 test
 
 
-# In[20]:
+# In[87]:
 
 
 result = locate_card(test['input']['cards'] , test['input']['query'])
 result
 
 
-# In[21]:
+# In[88]:
 
 
 result == output
 
 
-# In[22]:
+# In[89]:
 
 
 get_ipython().system('pip install jovian --upgrade --quiet')
 
 
-# In[23]:
+# In[90]:
 
 
 from jovian.pythondsa import evaluate_test_case
 
 
-# In[24]:
+# In[91]:
 
 
 evaluate_test_case(locate_card , test)
 
 
-# In[25]:
+# In[92]:
 
 
 from jovian.pythondsa import evaluate_test_cases
 
 
-# In[26]:
+# In[93]:
 
 
 evaluate_test_cases(locate_card , tests)
 
 
-# In[ ]:
+# In[94]:
 
 
-def locate_card(cards, query):
+def locate_card_linear(cards, query):
     position = 0 
     print('cards:',cards)
     print('query:',query)
@@ -266,7 +253,7 @@ def locate_card(cards, query):
 
 
 
-# In[ ]:
+# In[95]:
 
 
 cards6 = tests[6]['input']['cards']
@@ -274,10 +261,10 @@ query6 = tests[6]['input']['query']
 locate_card(cards6,query6)
 
 
-# In[ ]:
+# In[96]:
 
 
-def locate_card(cards, query):
+def locate_card_linear(cards, query):
     position = 0
 
     while position<len(cards) :
@@ -287,20 +274,20 @@ def locate_card(cards, query):
     return -1   
 
 
-# In[ ]:
+# In[97]:
 
 
 tests[6]
 
 
-# In[ ]:
+# In[98]:
 
 
 import os
 print(os.getcwd())
 
 
-# In[ ]:
+# In[99]:
 
 
 def locate_card(cards,query):
@@ -308,8 +295,7 @@ def locate_card(cards,query):
     while lo<=hi:
         mid = (lo+hi)//2
         mid_number = cards[mid]
-        print('low:', lo , 'high:', hi,'mid:',mid , 
-              'mid_number:',mid_number)
+
 
         if mid_number == query:
             return mid
@@ -327,13 +313,13 @@ def locate_card(cards,query):
 
 
 
-# In[ ]:
+# In[100]:
 
 
 evaluate_test_cases(locate_card,tests)
 
 
-# In[ ]:
+# In[101]:
 
 
 def test_location(cards , query , mid):
@@ -352,7 +338,7 @@ def locate_card(cards,query):
     lo,hi = 0,len(cards)-1
     while lo<=hi:
 
-        print('low:', lo , 'high:', hi, )
+
         mid = (lo+hi)//2
         result = test_location(cards , query , mid)
 
@@ -371,16 +357,94 @@ def locate_card(cards,query):
 
 
 
-# In[27]:
+# In[102]:
 
 
 evaluate_test_case(locate_card,tests[8])
 
 
-# In[ ]:
+# In[103]:
+
+
+def locate_card_linear(cards, query):
+    position = 0
+
+    while position<len(cards) :
+        if cards[position] == query:
+            return position
+        position +=1
+    return -1   
+
+
+# In[104]:
+
+
+large_test = {
+    'input': {
+        'cards':list(range(10000000,0,-1)) ,
+        'query': 2,
+    } , 
+    'output':9999998
+}
+
+
+# In[105]:
+
+
+result, passed, runtime = evaluate_test_case(locate_card_linear,large_test,display = False)
+print("Result : {} \n Passed : {} \n Execution time : {} ms ".format(result,passed,runtime))
+
+
+# In[106]:
+
+
+result, passed, runtime = evaluate_test_case(locate_card,large_test,display = False)
+print("Result : {} \n Passed : {} \n Execution time : {} ms ".format(result,passed,runtime))
+
+
+# In[107]:
+
+
+def binary_search(lo,hi,condition):
+    while lo<=hi:
+        mid = (lo+hi)//2
+        result =condition(mid)
+        if result == 'found':
+            return mid
+        elif result == 'left':
+            hi = mid - 1
+        else:
+            lo = mid +1 
+    return -1
 
 
 
+# In[108]:
+
+
+def locate_card(cards,query):
+
+    def condition(mid):
+        if cards[mid] == query:
+            if  mid > 0 and cards[mid-1]== query:
+                return 'left'
+            else:
+                return 'found'
+        elif cards[mid] < query:
+            return 'left'
+        else:
+            return 'right'
+
+    return binary_search(0, len(cards)-1 , condition)        
+
+
+
+
+
+# In[109]:
+
+
+evaluate_test_cases(locate_card,tests)
 
 
 # In[ ]:
